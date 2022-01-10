43c1094c7f17b845ed36eaab270411ce67ee192f
jrandj <james.randjelovic@gmail.com>
2022-01-10 11:24:23 +1100

    Update functionality for optimisation

--- backtester.py
+++ backtester.py
@@ -250,6 +250,7 @@ class Backtester:
         """
         self.cerebro.broker.addcommissioninfo(self.comminfo)
         self.cerebro.broker.setcash(float(self.config['broker']['cash']))
+        # this needs to be aware of the ranges
         self.add_strategy_data()
         self.cerebro.addobservermulti(bt.observers.BuySell, barplot=True, bardist=0.0025)
         self.cerebro.addobserver(bt.observers.Broker)
@@ -260,13 +261,37 @@ class Backtester:
         elif self.config['global_options']['strategy'] == 'Crossover':
             self.cerebro.addstrategy(CrossoverStrategy, verbose=True, log_file='strategy_log.csv')
         elif self.config['global_options']['strategy'] == 'CrossoverPlus':
-            for ii in range(20, 60, 20):
-                for jj in range(50, 90, 20):
-                    for kk in range(20, 40, 20):
-                        for ll in range(60, 80, 20):
-                            for mm in range(14, 28, 7):
-                                self.cerebro.optstrategy(CrossoverPlusStrategy, sma1=ii, sma2=jj, RSI_crossover_low=kk,
-                                                         RSI_crossover_high=ll, RSI_period=mm)
+            if self.config['crossover_plus_strategy_options']['optimise'] == "True":
+                for ii in range(int(self.config['crossover_plus_strategy_options']['sma1_low']),
+                                int(self.config['crossover_plus_strategy_options']['sma1_high']),
+                                int(self.config['crossover_plus_strategy_options']['sma1_step'])):
+                    for jj in range(int(self.config['crossover_plus_strategy_options']['sma2_low']),
+                                    int(self.config['crossover_plus_strategy_options']['sma2_high']),
+                                    int(self.config['crossover_plus_strategy_options']['sma2_step'])):
+                        for kk in range(int(self.config['crossover_plus_strategy_options']['RSI_crossover_low_low']),
+                                        int(self.config['crossover_plus_strategy_options']['RSI_crossover_low_high']),
+                                        int(self.config['crossover_plus_strategy_options']['RSI_crossover_low_step'])):
+                            for ll in range(
+                                    int(self.config['crossover_plus_strategy_options']['RSI_crossover_high_low']),
+                                    int(self.config['crossover_plus_strategy_options']['RSI_crossover_high_high']),
+                                    int(self.config['crossover_plus_strategy_options']['RSI_crossover_high_step'])):
+                                for mm in range(int(self.config['crossover_plus_strategy_options']['RSI_period_low']),
+                                                int(self.config['crossover_plus_strategy_options']['RSI_period_high']),
+                                                int(self.config['crossover_plus_strategy_options']['RSI_period_step'])):
+                                    self.cerebro.optstrategy(CrossoverPlusStrategy, sma1=ii, sma2=jj,
+                                                             RSI_crossover_low=kk,
+                                                             RSI_crossover_high=ll, RSI_period=mm)
+            else:
+                self.cerebro.addstrategy(CrossoverPlusStrategy, verbose=True, log_file='strategy_log.csv')
+            # for ii in range(20, 60, 20):
+            #     for jj in range(50, 90, 20):
+            #         for kk in range(20, 40, 20):
+            #             for ll in range(60, 80, 20):
+            #                 for mm in range(14, 28, 7):
+            #                     self.cerebro.optstrategy(CrossoverPlusStrategy, sma1=ii, sma2=jj,
+            #                                              RSI_crossover_low=kk,
+            #                                              RSI_crossover_high=ll, RSI_period=mm)
+            # self.cerebro.addstrategy(CrossoverPlusStrategy, verbose=True, log_file='strategy_log.csv')
         else:
             raise ValueError(f"Strategy {self.config['global_options']['strategy']} must be Pump or Crossover.")
         self.cerebro.addsizer(CustomSizer, percents=float(self.config['global_options']['position_size']))
@@ -415,14 +440,18 @@ class Backtester:
         # self.find_correlation()
 
         # run the strategy
-        self.cerebro = bt.Cerebro(stdstats=False)
+        self.cerebro = bt.Cerebro(stdstats=False, optreturn=False)
         if self.config['global_options']['cheat_on_close'] == 'True':
             self.cerebro.broker.set_coc(True)
         self.strategy_results = self.run_strategy()
         self.returns, self.positions, self.transactions, self.gross_lev = [None] * len(self.strategy_results[0]), [
             None] * len(self.strategy_results[0]), [None] * len(self.strategy_results[0]), [None] * len(
             self.strategy_results[0])
-        for idx, val in enumerate(self.strategy_results[0]):
+        if type(self.strategy_results[0]) is list:
+            object_for_iteration = self.strategy_results[0]
+        else:
+            object_for_iteration = self.strategy_results
+        for idx, val in enumerate(object_for_iteration):
             self.portfolio_stats = val.analyzers.getbyname('pyfolio')
             self.returns[idx], self.positions[idx], self.transactions[idx], self.gross_lev[idx] \
                 = self.portfolio_stats.get_pf_items()
