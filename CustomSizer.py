7693abb793e9d7bc4a3486118cb620dd1bf4b45c
jrandj <james.randjelovic@gmail.com>
2022-10-16 19:33:00 +1100

    Add functionality to prevent small orders

--- CustomSizer.py
+++ CustomSizer.py
@@ -1,23 +1,13 @@
 import configparser
 
 import backtrader as bt
+import math
 
 
 class CustomSizer(bt.Sizer):
     """
     A class that contains the logic for the order sizing.
 
-    Attributes
-    ----------
-    config : configparser.RawConfigParser
-        The object that will read configuration from the configuration file.
-    params : tuple
-        Parameters for the sizer.
-
-    Methods
-    -------
-    _getsizing()
-        Returns the number of shares to purchase.
     """
     config = configparser.RawConfigParser()
     config.read('config.properties')
@@ -29,29 +19,28 @@ class CustomSizer(bt.Sizer):
     )
 
     def _getsizing(self, comminfo, cash, data, isbuy):
-        """Returns the number of shares to purchase.
-
-        Parameters
-        ----------
-        comminfo : CustomCommissionScheme.CustomCommissionScheme
-            The commission scheme.
-        cash : float
-            The available cash.
-        data : TickerData.TickerData
-            The asset being considered for the order.
-        isbuy : bool
-            A flag indicating if the order is a buy order.
-
-        Raises
-        ------
-
         """
-        # use a percentage of the starting cash if possible (to keep the total amount neat)
-        if cash > (self.params.starting_cash * self.params.percents) / 100:
-            size = (self.params.starting_cash * self.params.percents) / (100 * data.close[0])
-        # otherwise use a percentage of the remaining cash
-        else:
-            size = (cash * self.params.percents) / (data.close[0] * 100)
+        Returns the number of shares to purchase.
+
+        :param comminfo: CustomCommissionScheme.CustomCommissionScheme.
+        :type comminfo: The commission scheme.
+        :param cash: The available cash.
+        :type cash: Float.
+        :param data: The asset being considered for the order.
+        :type data: TickerData.TickerData.
+        :param isbuy: A flag indicating if the order is a buy order.
+        :type isbuy: Bool.
+        :return: The size of the order.
+        :rtype: Float or Int.
+        """
+        size = math.floor((cash * self.params.percents) / (data.close[0] * 100))
+
+        # e.g. the order must be at least 2.5% for 5% position sizing (prevents orders with low amounts of cash)
+        if round(100 * size * data.close[0] / self.broker.get_value(), 2) < self.params.percents / 2:
+            # print(f"Did not open position for {data._name} as it is a small position with"
+            #       f" {round(100 * size * data.close[0] / self.broker.get_value(), 2)}% of the portfolio value, "
+            #       f"less than the limit {self.params.percents / 2}%")
+            size = 0
 
         if self.params.retint:
             size = int(size)
