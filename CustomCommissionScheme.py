72cda3f3f56f6da6b782c83d0529a49b7c0711d6
jrandj <james.randjelovic@gmail.com>
2022-10-21 15:58:03 +1100

    Update brokerage fees

--- CustomCommissionScheme.py
+++ CustomCommissionScheme.py
@@ -4,43 +4,33 @@ import backtrader as bt
 class CustomCommissionScheme(bt.CommInfoBase):
     """
     A class that sets the commission for the broker.
-
-    Attributes
-    ----------
-    params : tuple
-        Parameters for the commission scheme.
-
-    Methods
-    -------
-    _getcommission()
-        The method that provides the implementation for calculating the commission.
     """
+
     # parameters for the commission scheme
     params = (
         ('stocklike', True),
-        ('commtype', bt.CommInfoBase.COMM_FIXED),
+        ('commtype', bt.CommInfoBase.COMM_FIXED)
     )
 
     def _getcommission(self, size, price, pseudoexec):
-        """The method that provides the implementation for calculating the commission.
+        """
+        The method that provides the implementation for calculating the commission.
 
-        Parameters
-        ----------
-        size : float
-            The number of shares.
-        price : float
-            The price per share.
-        pseudoexec : bool
-            Indicates whether the call corresponds to the actual execution of an order.
-
-        Raises
-        ------
-
+        :param size: The number of shares.
+        :type size: Float.
+        :param price: The price per share.
+        :type price:  Float.
+        :param pseudoexec: Indicates whether the call corresponds to the actual execution of an order.
+        :type pseudoexec: Bool.
+        :return: The commission to be paid.
+        :rtype: Float.
         """
         value = abs(size) * price
-        if value <= 5000:
+        if value < 1000:
+            return 9.95
+        elif value <= 5000:
             return 14.95
-        elif 5000 < value < 20000:
+        elif value <= 20000:
             return 19.95
         else:
             return 0.0011 * value
